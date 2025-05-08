58c669cd45c45dad54d2f74ccfb44d402b2861ec
claudia rivas <usr3860@salesianos-lacuesta.net>
2025-05-08 21:04:32 +0100

    feat: Add RecipesPage empty

---
+++ app/(drawer)/recipes/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RececipesPage = () => {
+  return (
+    <View>
+      <Text>Recipes</Text>
+    </View>
+  )
+}
+
+export default RececipesPage
+
+const styles = StyleSheet.create({})

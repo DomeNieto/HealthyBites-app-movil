b7f3d987ce3031a6b394e1a081123caa2ebcdf04
claudia rivas <usr3860@salesianos-lacuesta.net>
2025-05-22 23:46:46 +0100

    refactor: Refactoring getAllIngredients function

--- services/ingredient-service.ts
+++ services/ingredient-service.ts
@@ -1,17 +1,37 @@
-const API_URL = "http://192.168.0.123:8082/api/v1/ingredients";
+import axios from "axios";
+import { getToken } from "./async-storage-service";
+
+const API_URL = "http://192.168.0.18:8082/api/v1/ingredients/active";
+
+/**
+ * The function `getAllIngredients` asynchronously fetches data from an API URL and returns an array of
+ * objects with `id` and `name` properties, handling errors gracefully.
+ * @returns The `getAllIngredients` function is returning an array of objects with `id` as a number and
+ * `name` as a string. If there is an error during the fetching process, an empty array will be
+ * returned.
+ */
+type Ingredient = { id: number; name: string };
 
 const getAllIngredients = async () => {
+  const token = await getToken();
+  const cleanedToken = token!.replace(/['"]+/g, "");
+
   try {
-    const res = await fetch(`${API_URL}`);
-    const json = await res.json();
-    if (!res.ok) throw new Error(`HTTP ${res.status}`);
-    return json.data as { id: number; name: string }[];
-  } catch (e) {
-    console.error("getAllIngredients:", e);
+    const res = await axios.get<{ data: Ingredient[] }>(
+      `${API_URL}`,
+      {
+        headers: {
+          Authorization: `Bearer ${cleanedToken}`,
+        },
+      }
+    );
+
+    return res.data.data;
+  } catch (error) {
+    console.error("getAllIngredients unexpected error:", error);
     return [];
   }
 };
-
 
 const ingredientsService = {
   getAllIngredients,
